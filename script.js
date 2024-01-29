103bfc781f8339ea34d970bb545aa92c1f23ee0c
Sharma Pravin <142722915+known-27@users.noreply.github.com>
2024-01-29 08:04:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const quotes = [
     { text: "In three words I can sum up everything I've learned about life: it goes on.", author: "Robert Frost" },
     { text: "Be yourself; everyone else is already taken.", author: "Oscar Wilde" },
     { text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
-    { text: "The only limit to our realization of tomorrow will be our doubts of today.", author: "Franklin D. Roosevelt" }
+    { text: "The only limit to our realization of tomorrow will be our doubts of today.", author: "Franklin D. Roosevelt" },
+    { text: "To live is the rarest thing in the world. Most people exist, that is all.", author: "Oscar Wilde"},
+    { text: "That it will never come again is what makes life so sweet.", author: "Emily Dickinson"},
+    { text: "It is never too late to be what you might have been.", author: " George Eliot"},
+    { text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.", author: "Ralph Waldo Emerson"},
+    { text: "Be kind, for everyone you meet is fighting a hard battle.", author: " Plato"},
+    { text: "We are all broken, that's how the light gets in.", author: "Ernest Hemingway"}
   ];
   
   const quoteContainer = document.getElementById('quote-container');
